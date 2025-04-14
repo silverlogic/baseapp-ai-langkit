963cfa284512a08266bfa450570621a9a937bfee
Hercílio M. Ortiz <herciliomartins@gmail.com>
2025-04-14 14:18:03 -0300

    Bugfix: Adjusting package to be used by CAMP

--- baseapp_ai_langkit/rest_framework/router.py
+++ baseapp_ai_langkit/rest_framework/router.py
@@ -7,7 +7,6 @@ from baseapp_ai_langkit.chats.rest_framework.views import (
 )
 
 baseapp_ai_langkit_router = routers.DefaultRouter()
-# TODO: (when the FE chat is connected to Campaign Chat) figure out how to disable the chat routes without blocking the unit tests.
 baseapp_ai_langkit_router.register(r"chat", BaseChatViewSet, basename="llm-chat")
 baseapp_ai_langkit_router.register(
     r"chat/identity", ChatIdentityViewSet, basename="llm-chat-identity"
--- baseapp_ai_langkit/tests/factories.py
+++ baseapp_ai_langkit/tests/factories.py
@@ -3,11 +3,14 @@ from django.contrib.auth import get_user_model
 
 
 class UserFactory(factory.django.DjangoModelFactory):
-    username = factory.Faker("user_name")
     email = factory.Faker("email")
     password = factory.PostGenerationMethodCall("set_password", "default")
     first_name = factory.Faker("first_name")
     last_name = factory.Faker("last_name")
+
+    # Dynamically add username if it exists in the model
+    if "username" in get_user_model()._meta.get_fields():
+        username = factory.Faker("user_name")
 
     class Meta:
         model = get_user_model()
